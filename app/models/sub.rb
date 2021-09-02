df6a9eaf5358d328e71f95b637d8af3352ff3ffc
Tyler Case <tcase629@yahoo.com>
2021-09-01 19:20:58 -0600

    add models and routes

---
+++ app/models/sub.rb
@@ -0,0 +1,3 @@
+class Sub < ApplicationRecord
+  validates :name, presence: true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # root
+  root 'subs#index'
+  # get 'sub#new'
+  # http verb 'controller#action'
+  # post 'subs#create'
+
+  # resources 
+  resources :subs
+
+  # to access it would be localhost:3000/rails/info/routes, with your server running
 end
